ca219073b416cc969a1693c8cd48e5dfc7df8776
Amrik <ztriplezero@hotmail.com>
2020-01-09 14:39:21 -0500

    Initial Commit

---
+++ Fathce/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fathce
+//
+//  Created by Z Angrazy Jatt on 2020-01-09.
+//  Copyright © 2020 Psych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

31aeebca13fd711000940fd51c946cd5780c40c8
Amrik <ztriplezero@hotmail.com>
2020-01-20 16:17:27 -0500

    Adding menu to each view controller to navigate user directly.

--- Fathce/ProfileViewController.swift
+++ Fathce/ProfileViewController.swift
@@ -9,9 +9,49 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    let transition = MenuSlideTransition()
+    var topView:UIView?
     @IBAction func backButtonClicked(_ sender: UIBarButtonItem) {
-        
-        guard let nextView = storyboard?.instantiateViewController(identifier: "HomeView") else {return}
+        // print(menuType)
+        guard let menuViewController = storyboard?.instantiateViewController(identifier: "menuViewController") as? MenuViewController else {return}
+               menuViewController.didMenutype = {
+                    menuType in
+                   self.transitionToContent(menuType)
+               }
+               menuViewController.modalPresentationStyle = .overCurrentContext
+        menuViewController.transitioningDelegate = self as? UIViewControllerTransitioningDelegate
+               present(menuViewController, animated: true)
+    }
+    
+     func transitionToContent(_ menuType: MenuType) {
+            let title = String(describing: menuType).capitalized
+            self.title = title
+            topView?.removeFromSuperview()
+            switch menuType {
+            case .profile:
+                 nextView()
+                
+                case .therapyCenter:
+                let view = UIView()
+                view.backgroundColor = .blue
+                view.frame = self.view.bounds
+                self.view.addSubview(view)
+                self.topView = view
+                
+                case .home:
+                let view = UIView()
+                view.backgroundColor = .yellow
+                view.frame = self.view.bounds
+                self.view.addSubview(view)
+                self.topView = view
+            default:
+                break
+            }
+        }
+    
+    func nextView()
+    {
+        guard let nextView = storyboard?.instantiateViewController(identifier: "ProfileViewController") else {return}
         nextView.modalPresentationStyle = .overCurrentContext
         nextView.transitioningDelegate = nil
         present(nextView, animated: true)
